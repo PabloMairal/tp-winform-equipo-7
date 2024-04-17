b37d5ef6644f73724e21ed51d11ad85a911fa694
Emanuel <emanuelmonardo@gmail.com>
2024-04-16 21:02:17 -0300

    Creación ventana, listado de articulos

--- Catalogo/Form1.Designer.cs
+++ Catalogo/Form1.Designer.cs
@@ -1,16 +1,15 @@
-﻿namespace Catalogo
-{
+﻿namespace Catalogo {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// Variable del diseñador necesaria.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Limpiar los recursos que se estén usando.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +19,163 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAgregarArticulo = new System.Windows.Forms.Button();
+            this.txtBuscador = new System.Windows.Forms.TextBox();
+            this.lvArticulos = new System.Windows.Forms.ListView();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.btnDetalles = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mARCASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cODIGOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAgregarArticulo
+            // 
+            this.btnAgregarArticulo.Location = new System.Drawing.Point(450, 103);
+            this.btnAgregarArticulo.Name = "btnAgregarArticulo";
+            this.btnAgregarArticulo.Size = new System.Drawing.Size(119, 29);
+            this.btnAgregarArticulo.TabIndex = 0;
+            this.btnAgregarArticulo.Text = "Agregar";
+            this.btnAgregarArticulo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnAgregarArticulo.UseVisualStyleBackColor = true;
+            this.btnAgregarArticulo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBuscador
+            // 
+            this.txtBuscador.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscador.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtBuscador.Location = new System.Drawing.Point(166, 2);
+            this.txtBuscador.Name = "txtBuscador";
+            this.txtBuscador.Size = new System.Drawing.Size(852, 22);
+            this.txtBuscador.TabIndex = 3;
+            this.txtBuscador.Text = "Buscar Articulo";
+            // 
+            // lvArticulos
+            // 
+            this.lvArticulos.HideSelection = false;
+            this.lvArticulos.Location = new System.Drawing.Point(220, 187);
+            this.lvArticulos.Name = "lvArticulos";
+            this.lvArticulos.Size = new System.Drawing.Size(730, 394);
+            this.lvArticulos.TabIndex = 4;
+            this.lvArticulos.UseCompatibleStateImageBehavior = false;
+            this.lvArticulos.View = System.Windows.Forms.View.List;
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(632, 108);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(119, 20);
+            this.txtItem.TabIndex = 5;
+            // 
+            // btnDetalles
+            // 
+            this.btnDetalles.Location = new System.Drawing.Point(220, 672);
+            this.btnDetalles.Name = "btnDetalles";
+            this.btnDetalles.Size = new System.Drawing.Size(119, 29);
+            this.btnDetalles.TabIndex = 6;
+            this.btnDetalles.Text = "Detalles";
+            this.btnDetalles.UseVisualStyleBackColor = true;
+            this.btnDetalles.Click += new System.EventHandler(this.btnDetalles_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(524, 672);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(119, 29);
+            this.btnModificar.TabIndex = 7;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(831, 672);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(119, 29);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mARCASToolStripMenuItem,
+            this.cODIGOToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1226, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mARCASToolStripMenuItem
+            // 
+            this.mARCASToolStripMenuItem.Name = "mARCASToolStripMenuItem";
+            this.mARCASToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.mARCASToolStripMenuItem.Text = "MARCAS";
+            // 
+            // cODIGOToolStripMenuItem
+            // 
+            this.cODIGOToolStripMenuItem.Name = "cODIGOToolStripMenuItem";
+            this.cODIGOToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.cODIGOToolStripMenuItem.Text = "CODIGO";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1226, 24);
+            this.menuStrip2.TabIndex = 10;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1226, 824);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnDetalles);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.lvArticulos);
+            this.Controls.Add(this.txtBuscador);
+            this.Controls.Add(this.btnAgregarArticulo);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAgregarArticulo;
+        private System.Windows.Forms.TextBox txtBuscador;
+        private System.Windows.Forms.ListView lvArticulos;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.Button btnDetalles;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem mARCASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cODIGOToolStripMenuItem;
     }
 }
 
--- Catalogo/Form1.cs
+++ Catalogo/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
 
-namespace Catalogo
-{
+namespace Catalogo {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string articulo = txtItem.Text;
+            lvArticulos.Items.Add(articulo);
+        }
+
+        private void btnDetalles_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
