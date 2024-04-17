f236c15ca0164060dc8620c1a4c40fda80430d31
PabloMairal <pablomairal@gmail.com>
2024-04-17 14:11:34 -0300

    Clase Marca y archivo de configuración

---
+++ Catalogo/Marca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo
+{
+    internal class Marca
+    {
+        public Marca(int Id, string Nombre) { 
+            this.Id = Id;
+            this.Nombre = Nombre;
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
