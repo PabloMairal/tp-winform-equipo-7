3670ff61e979e8ab0f82e4f8d9b14473ea8bcf0c
Pablo Pelardas <pablo.pelardas@gmail.com>
2024-04-16 21:23:21 -0300

    Clase categoría con propiedades y constructor

---
+++ Catalogo/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo
+{
+    internal class Categoria
+    {
+        public Categoria(int id, string nombre)
+        {
+            Id = id;
+            Nombre = nombre;
+        }
+
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }  
+
+        public override string ToString()
+        {
+            return $"{Id} - {Nombre}";
+        }
+
+    }
+}
